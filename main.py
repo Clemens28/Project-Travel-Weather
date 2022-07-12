b7e220da732cfca3a37d6a0b50035c7543ce6f59
Wenzel-Ruelberg <A19E317@rewe-group.com>
2022-07-12 14:45:31 +0200

    Neue Änderungen, um Datum zu formatieren

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ data = Monthly(location, start, end)
 data = data.fetch()
 
 # Plot line chart including average, minimum and maximum temperature
-data.plot(y=['tavg', 'tmin', 'tmax'])
-plt.show()
+#data.plot(y=['tavg', 'tmin', 'tmax'])
+#plt.show()
 
 
 
@@ -41,3 +41,23 @@ print(df1.dtypes)
 
 ## cut the year and the day from the timestamp
 
+mid=df1['time'].str[5:7]
+
+#mid.columns=['month']
+
+#mid= mid.rename(columns={'time': 'month'})
+
+#print(mid)
+
+#print(mid.dtypes)
+
+df2= pd.concat([df1, mid], axis=1, join="inner")
+
+print(df2.columns)
+df2= df2.rename(columns={df2.columns[8]: 'month'})
+
+print(df2)
+print (df2.dtypes)
+#df2 = df2.drop(df2.columns[(8)], axis=1)
+print(df2)
+#print(df3)
