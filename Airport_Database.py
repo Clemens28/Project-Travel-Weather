edcf57660f49e6dfb434b60946f7b5afa75b396a
Wenzel-Ruelberg <A19E317@rewe-group.com>
2022-07-15 16:20:47 +0200

    For Loop implementiert: In Zeile 48 Bug mit dt gefunden

--- Airport_Database.py
+++ Airport_Database.py
@@ -1,5 +1,18 @@
 import pandas as pd
 
-df10 = pd.read_csv('GlobalAirportDatabase.csv', sep=';')
+df10 = pd.read_csv('GlobalAirportDatabase_Updated_v0.1.csv', sep=';')
 print (df10)
 
+location_lat=df10.iat[0,3]
+location_lon=df10.iat[0,4]
+
+print(location_lat)
+print(location_lon)
+
+#df10.astype({'LAT':float,'LON':float},errors='raise')
+
+print(df10.dtypes)
+
+
+for 'LAT' in df10:
+  print('LAT')
