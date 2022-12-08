8816e8ee8e90fcdf0204dc68309e6b26f0904aa8
Wenzel-Ruelberg <A19E317@rewe-group.com>
2022-12-08 16:19:45 +0100

    Bar Charts improved

--- 10_The_Best_One_So_Far.py
+++ 10_The_Best_One_So_Far.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 import pydeck as pdk
+import plotly.express as px 
 
 df = pd.read_csv('List_of_Weather_Data_Cleaned_ALL_withLocation_v01.csv', sep=';')
 #print (df21)
@@ -42,16 +43,33 @@ st.title(f"Where to travel next?")
 st.markdown('Average Temperatures')
 
 st.dataframe(data=df.sort_values('AVG_TEMP',
-             ascending=False).reset_index(drop=True), width=10000)
+             ascending=False).reset_index(drop=True), use_container_width=True)
 
 
 # Main - charts
 
 st.markdown('Chart')
 
-#df.set_index(df['CITY'])
-df['CITY'] = df.index
-st.bar_chart(data=df['AVG_TEMP'])
+df.set_index(df['CITY'])
+#df['CITY'] = df.index
+
+#chart_data1=pd.DataFrame(df,rotation='horizontal')
+#st.bar_chart(data=df['AVG_TEMP'])
+df = df[df['MONTH'].isin(month_choice)]
+
+fig1=px.bar(df,x='AVG_TEMP',y='CITY', orientation='h', width=650, height=500)
+#st.write(fig1)
+
+fig2=px.bar(df,x='PRCP',y='CITY', orientation='h', width=500, height=500)
+
+col1,col2=st.columns(2)
+with col1:
+        st.header('AVG Temperature')
+        st.write(fig1)
+with col2:
+        st.header('AVG Precipitation')
+        st.write(fig2)
+
 
 st.markdown('World Map')
 
